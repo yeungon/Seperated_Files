a9c028a6081e5b736d5c92e09d6a9d291aac812e
Vuong Nguyen <yeungon@gmail.com>
2018-12-04 09:13:53 -0600

    Add files via upload

---
+++ done.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+<br>
+
+<?php
+
+echo "completed";
+
+?>
+
+<br>
+
+
+<a href="/www/file"><button>back</button></a>
+</body>
+</html>
